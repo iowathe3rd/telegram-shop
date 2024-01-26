24ed524f874f5022fe025cc4e281c345e142e73b
iowathe3rd <iowathe3rd@proton.me>
2024-01-26 20:38:19 +0600

    style(project): configured prettier config

---
+++ apps/client/src/components/ProductItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "@mui/material/Card"
+
+const ProductItem: React.FC = () => {
+    return (
+        <Card >
+            
+        </Card>
+    )
+}
+
+export default ProductItem;
