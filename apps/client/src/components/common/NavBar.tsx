7a3e40272ec564fdc5a807fb53bdd09521897b6b
iowathe3rd <iowathe3rd@proton.me>
2024-01-30 00:55:38 +0600

    chore(client): extracted basket dropdown to external component

--- apps/client/src/components/common/NavBar.tsx
+++ apps/client/src/components/common/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useBasketStore } from '../../store';
+import BasketDropdown from '../BasketDropdown';
 
 const NavBar: React.FC = () => {
 	const { products, totalPrice } = useBasketStore();
@@ -10,49 +11,10 @@ const NavBar: React.FC = () => {
 		<div className='navbar tg-header-bg-colors'>
 			<div className='flex-1'></div>
 			<div className='flex-none'>
-				<div className='dropdown dropdown-end dropdown-hover'>
-					<div
-						tabIndex={0}
-						role='button'
-						className='btn btn-ghost btn-circle'
-					>
-						<button></button>
-						<div className='indicator'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								className='h-5 w-5'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke='currentColor'
-							>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									strokeWidth='2'
-									d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-								/>
-							</svg>
-							<span className='badge badge-sm indicator-item'>{products.length}</span>
-						</div>
-					</div>
-					<div
-						tabIndex={0}
-						className='mt-3 z-[1] card card-compact dropdown-content tg-section-bg-color w-52 shadow'
-					>
-						<div className='card-body'>
-							<span className='font-bold text-lg'>{products.length} Items</span>
-							<span className='text-info'>Subtotal: ${totalPrice()}</span>
-							<div className='card-actions'>
-								<button
-									className='btn btn-primary btn-block'
-									onClick={() => window.basket_modal.show()}
-								>
-									View cart
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
+				<BasketDropdown
+					basketProducts={products}
+					totalPrice={totalPrice}
+				/>
 			</div>
 		</div>
 	);
